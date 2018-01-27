0efb930c76cf8879c74d1f0d77d2a451a9ee9eea
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2018-01-27 12:04:18 -0800

    added locations migration lol

--- database/migrations/2018_01_27_192957_create_locations_table.php
+++ database/migrations/2018_01_27_192957_create_locations_table.php
@@ -14,7 +14,20 @@ class CreateLocationsTable extends Migration
     public function up()
     {
         Schema::create('locations', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('location_id');
+            $table->string('g_location_id')->nullable(false);
+            $table->string('location_name')->nullable(false);
+            $table->string('categories')->nullable(false);
+            $table->string('rating')->nullable(false);
+            $table->string('latitude')->nullable(false);
+            $table->string('longitude')->nullable(false);
+            $table->string('price')->nullable(false);
+            $table->string('address')->nullable(false);
+            $table->string('city')->nullable(false);
+            $table->string('zip_code')->nullable(false);
+            $table->string('country')->nullable(false);
+            $table->string('state')->nullable(false);
+            $table->string('display_phone')->nullable(false);
             $table->timestamps();
         });
     }
