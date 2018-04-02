ea75aeb00651efd8849a1ddecfd940c672fd79b7
b-ubben <brackben@gmail.com>
2018-04-01 18:28:36 -0700

    big changes to register page

--- resources/assets/js/components/pages/Register.js
+++ resources/assets/js/components/pages/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { string } from 'prop-types';
 
 //import partials
@@ -21,7 +22,9 @@ export default class Register extends Component {
     validPassword: false,
     validPasswordConf: false,
     valid: false,
-    isLoggedIn: false
+    isLoggedIn: false,
+    failedRegistration: false,
+    successfulRegistration: false
   }
 
   static defaultProps = {
@@ -32,11 +35,6 @@ export default class Register extends Component {
     baseUrl: string.isRequired
   }
 
-    /*
-    *  setState is asynchronous, so if you want to print the new state, you have to use the callback parameter.
-    *  this means that in setState, you do a callback function that will actually handle the validation.
-    *  before, the wrong length was being thrown out and it was skipping the last character entered. this resolves that problem.
-    */
   validateUsername = (e) => {
     this.setState({username: e.target.value}, () => {
 
@@ -60,10 +58,8 @@ export default class Register extends Component {
     var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     this.setState({email: e.target.value}, () => {
       if(this.state.email.match(emailRegex)) {
-        console.log("Valid email!!!: " + this.state.email);
         this.setState({validEmail: true});
       } else {
-        console.log("Email: " + this.state.email + " is invalid. Enter another one!!");
         this.setState({validEmail: false});
       }
     });
@@ -72,11 +68,9 @@ export default class Register extends Component {
   validatePassword = (e) => {
     this.setState({password: e.target.value}, () => {
       if (this.state.password.length >= 5) {
-        console.log("Valid!");
         this.setState({validPassword: true});
       }
       else {
-        console.log("Password must be greater than or equal to 5 chars");
         this.setState({validPassword: false});
       }
     });
@@ -85,10 +79,8 @@ export default class Register extends Component {
   validatePasswordConf = (e) => {
     this.setState({passwordConf: e.target.value}, () => {
       if(this.state.password == this.state.passwordConf) {
-        console.log("Password Confirmation Confirmed!!!!");
         this.setState({validPasswordConf: true});
       } else {
-        console.log("Password confirmation failed :(");
         this.setState({validPasswordConf: false});
       }
     });
@@ -98,22 +90,29 @@ export default class Register extends Component {
   readyForSubmission = (e) => {
     if(this.state.validUsername && this.state.validEmail &&
       this.state.validPassword && this.state.validPasswordConf) {
+      //must be here to stop from page reload after submit. Redirection must be handled by router..
+      e.preventDefault();
 
       this.setState({valid: true}, () => {
         // code goes here for fetch to submit all parameters in the state.
         let loginData = JSON.stringify({
             username: this.state.username,
             email: this.state.email,
-            password: this.state.password,
+            password: this.state.password
         })
-
         // run the post request with the user loginData shown above
         axios.post(this.props.baseUrl + '/register', loginData, {
               headers: {
                   'Content-Type': 'application/json',
               }
-          }
-        )
+          }).then(function(response) {
+            if (response.status === '200') {
+              this.setState({successfulRegistration: true});
+            }
+          }).catch((error) => {
+            this.setState({failedRegistration: true});
+            console.log('failed registration');
+          });
       });
     } else {
       // disable enter key/go button submission if requirements aren't met.
@@ -126,8 +125,13 @@ export default class Register extends Component {
     const validEmail = this.state.validEmail;
     const validPassword = this.state.validPassword;
     const validPasswordConf = this.state.validPasswordConf;
+    const valid = this.state.valid;
+    const failedRegistration = this.state.failedRegistration;
+    const successfulRegistration = this.state.successfulRegistration;
 
     return(
+      successfulRegistration ? <Redirect to="/" /> :
+
       <div>
         <Navigation isLoggedIn={ this.state.isLoggedIn }/>
         <Messages />
@@ -151,7 +155,11 @@ export default class Register extends Component {
                 <input type="password" name="passwordConf" id="passwordConf" placeholder="Please confirm password" className="input-long" onChange={ this.validatePasswordConf } />
                 { validPasswordConf ? <p className="small margin-top-nothing">Passwords Match <FontAwesomeIcon icon={faCheckCircle} color="green"/></p> : <p className="small margin-top-nothing">Please confirm your password by typing it again.</p> }
 
-          			<input type="submit" name="submit" value="Go" className="margin-x-auto margin-top-something margin-bottom-enough button-long bg-dank text-white" onClick={ this.readyForSubmission } />
+                <input type="submit" name="submit" value="Go" className="margin-x-auto margin-top-something margin-bottom-enough button-long bg-dank text-white" onClick={ this.readyForSubmission } />
+
+                {
+                    failedRegistration ? <p className="text-attention">Failed to register you. Please try again.</p> : <span className="display-none"></span>
+                }
         			</form>
         		</div>
         	</div>
