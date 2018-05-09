0c26b3589b6cffd55483c02ddae260aad5071327
b-ubben <brackben@gmail.com>
2018-05-08 18:02:01 -0700

    added ko-fi button to about page

--- resources/assets/js/components/pages/About.js
+++ resources/assets/js/components/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 //import partials
 import Navigation from './../partials/Navigation';
@@ -6,6 +7,14 @@ import Footer from './../partials/Footer';
 
 export default class About extends Component {
   render() {
+    const buttonStyle = {
+      display: 'block',
+      height: 'auto',
+      margin: 'auto',
+      maxWidth: '150px',
+      width: '100%'
+    }
+
     return(
       <div>
         <Navigation />
@@ -20,6 +29,12 @@ export default class About extends Component {
         		  <div className="pane pane-rounded bg-light padding-enough">
                 <div className="pane-body row">
                   <p>Drinq was previously developed and maintained by a senior design project team in California State University, Northridge's Computer Information Techonology program known as the Morning Drinkers. Now, primary development continues on as a passion project of the a few members of that team.</p>
+
+                  <p>Like what we've done here? Support us below!</p>
+
+                  <div className="item">
+                    <Link to='https://ko-fi.com/B0B2CUWF' target='_blank'><img src='https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0' style={ buttonStyle } alt='Buy Me a Coffee at ko-fi.com' /></Link>
+                  </div>
                 </div>
         		  </div>
         	 </section>
